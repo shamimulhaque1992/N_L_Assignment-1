f08e8b78d115d4830a252928b3858fe47a77873e
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2026-05-07 03:27:30 +0600

    problem-2: Reverse string

--- solutions.ts
+++ solutions.ts
@@ -13,3 +13,15 @@ const filterEvenNumbers = (input: number[]): number[] => {
 
 const resultOfProblem1 = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 
+// Problem 2:
+const reverseString = (input: string): string => {
+  let reversedString: string = "";
+
+  input.split("").forEach((char) => {
+    reversedString = char + reversedString;
+  });
+  
+  return reversedString;
+};
+
+const resultOfProblem2 = reverseString("typescript");
