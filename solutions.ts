295c3092ea790e913099a1fd6eba25ed7598b998
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2026-05-07 12:19:44 +0600

    problem-6: Inharitance - student inharited propeties of person and the method returns custom string

--- solutions.ts
+++ solutions.ts
@@ -48,3 +48,26 @@ interface Book {
 const toggleReadStatus = (input: Book): Book & { isRead: boolean } => {
   return { ...input, isRead: true };
 };
+
+// Problem 6:
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
